3da04486ee7feba05fb8de1a0e64c9afa7cee181
LamVanChuong04 <lamvanchuong04@gmail.com>
2025-07-17 15:53:57 +0700

    Bài tập Fibonacci

--- KiThuatLapTrinh/src/Baitap.java
+++ KiThuatLapTrinh/src/Baitap.java
@@ -35,6 +35,7 @@ public class Baitap {
         
      
         System.out.println(factorial(number));
+        System.out.println(fibo2(number));
     }
     // Bài tập 4: Tính giai thừa của n, nhập n từ bàn phím
     // c1: O(n) về thời gian, O(n) về bộ nhớ
@@ -51,5 +52,26 @@ public class Baitap {
             result *= i;
         }
         return result;
+    }
+    // Bài tập 5: Tìm số fibonacci thứ n, nhập n từ bàn phím
+    // c1: dùng đệ quy với công thức fibo(n-1) + fibo(n-2)
+    public static int fibo(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        return fibo(n-1) + fibo(n-2);
+    }
+    // c2: dùng vòng lặp for
+    public static int fibo2(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        int f1 = 1; 
+        int f2 = 1;
+        int f3 = 0;
+        for(int i = 2; i<n; i++){
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+        }
+        return f3;
     }
 }
